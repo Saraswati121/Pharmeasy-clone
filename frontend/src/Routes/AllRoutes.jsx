07c4b49cb6041ee06e7fb912ecc0eccd2d4e0a4e
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2022-08-23 19:35:04 +0530

    folder created

---
+++ frontend/src/Routes/AllRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+
+export const AllRoutes = () => {
+  return (
+    <div>
+        {/* Add routes here only. */}
+
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
+
+    </div>
+  )
+}
