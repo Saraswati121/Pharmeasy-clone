28676cc9496011580922a7d5a047e13e64d0a3b8
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-08-24 01:25:49 +0530

    Navbar Layout Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./Component/Home/Navbar";
 import AllRoutes from "./Routes/AllRoutes"
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <AllRoutes/>
     </div>
   );
