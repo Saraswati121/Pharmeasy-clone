fae948ce2ae18d3f0a00dd7831d41083616d5eb0
SumitB1412 <beniwalsumit1412@gmail.com>
2022-08-24 18:59:46 +0530

    complete products page layout

--- frontend/src/Component/ProductCard.jsx
+++ frontend/src/Component/ProductCard.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import styles from "../styles/productcard.module.css";
 
 export const ProductCard = () => {
+  const [discount, setDiscount] = useState("");
+
+  useEffect(() => {
+    const discountPrice = 270.5 - 216.4;
+    const discountPercent = (discountPrice / 270.5) * 100;
+    setDiscount(discountPercent);
+  }, []);
+
   return (
     <div className={styles.productCardParent}>
       <div className={styles.imgDiv}>
-      <img src="https://cdn01.pharmeasy.in/dam/products_otc/A75976/liveasy-essentials-handwash-pump-250-ml-2-1653312958.jpg" alt=""/>
+        <img
+          src="https://cdn01.pharmeasy.in/dam/products_otc/207833/calcimax-forte-plus-strip-of-30-tablets-2-1654078168.jpg"
+          alt="product_img"
+        />
       </div>
       <p>Calcimax Forte Plus Strip Of 30 Tablets</p>
-      <p className={styles.cutoffPrice}>MRP <span style={{textDecoration:"line-through"}}>₹270.5</span></p>
+      <div className={styles.discountBox}>
+        <p className={styles.cutoffPrice}>
+          MRP <span style={{ textDecoration: "line-through" }}>₹270.5</span>
+        </p>
+        <div className={styles.discountBadge}>{discount}% OFF</div>
+      </div>
       <p className={styles.newPrice}>₹216.4</p>
     </div>
   );
