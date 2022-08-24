de29f6545307f0d368269069760f4c731584b6c5
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-08-24 23:34:10 +0530

    done

--- frontend/src/Component/Home/Firstpart.jsx
+++ frontend/src/Component/Home/Firstpart.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import style from './firstpart.module.css'
-let data = [
-    {image:"https://assets.pharmeasy.in/apothecary/images/medicine_ff.webp?dim=256x0",name:"Medicine",discount:"Upto 50% OFF"},
-    {image:"https://assets.pharmeasy.in/apothecary/images/labtest_ff.webp?dim=256x0",name:"LabTest",discount:"Upto 70% OFF"},
-    {image:"https://assets.pharmeasy.in/apothecary/images/healthcare_ff.webp?dim=256x0",name:"Health Care",discount:"Upto 60% OFF"},
-    {image:"https://assets.pharmeasy.in/apothecary/images/health_blogs_ff.webp?dim=256x0",name:"Health Blogs",discount:""},
-    {image:"https://assets.pharmeasy.in/apothecary/images/plus_ff.webp?dim=256x0",name:"Plus",discount:"SAVE 5% EXTRA"},
-    {image:"https://assets.pharmeasy.in/apothecary/images/offers_ff.webp?dim=256x0",name:"Offers",discount:""},
-    {image:"https://assets.pharmeasy.in/apothecary/images/offers_1_ff.webp?dim=256x0",name:"Surgeries",discount:""},
-    {image:"https://assets.pharmeasy.in/apothecary/images/value_store.png?dim=256x0",name:"Value",discount:"Upto 50% OFF"},
-]
+import Data from './Data.json'
+
 const Firstpart = () => {
   return (
     <div>
         <div className={style.container}>
-          {data.map((el)=>(
+          {Data.image_data.map((el)=>(
             <div className={style.box} key={el.name}>
                 <div className={style.image_box}>
                     <img src={el.image} alt="" />
--- frontend/src/Component/Home/Homepage.jsx
+++ frontend/src/Component/Home/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import style from './homepage.module.css'
 import Firstpart from './Firstpart';
+import Carousal from './Carousal';
 const Homepage = () => {
   return (
     <div>
         <div className={style.main_container}>
           <Firstpart/>
+          <Carousal/>
         </div>
     </div>
   )
