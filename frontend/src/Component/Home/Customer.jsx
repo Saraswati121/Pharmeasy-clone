5f167f8dd78b4ffabef013c1c35d18941eeb9ab9
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-08-25 16:36:46 +0530

    customer slider done

--- frontend/src/Component/Home/Customer.jsx
+++ frontend/src/Component/Home/Customer.jsx
@@ -12,11 +12,11 @@ function NextArrow(props) {
         ...style,
         display: "block",
         color: "black",
-        height: "2rem",
-        marginRight: "1rem",
-        background: "none",
-        width: "2rem",
-        zIndex: 50
+        height: "3rem",
+        background: "#f2fff8",
+        width: "3rem",
+        zIndex: 50,
+        borderRadius:"50%"
       }}
       onClick={onClick}
     />
@@ -31,12 +31,12 @@ function PrevArrow(props) {
       style={{
         ...style,
         display: "block",
-        marginLeft: "1rem",
-        background: "none",
         color: "black",
-        height: "2rem",
-        width: "2rem",
-        zIndex: 50
+        height: "3rem",
+        background: "#f2fff8",
+        width: "3rem",
+        zIndex: 50,
+        borderRadius:"50%"
       }}
       onClick={onClick}
     />
@@ -45,37 +45,37 @@ function PrevArrow(props) {
 const db = [
   {
     name: "Darpan Dholakia",
-    date: "April 23, 2020",
+    date: "April 23, 2022",
     content:
       "Best service and app amongst all available. I have been using it for more than 3 years, and even during the pandemic, they have kept their standards high and are delivering the order within 24 hours. Keep up the good work."
   },
   {
     name: "Lipi Chaudhuri",
-    date: "April 15, 2020",
+    date: "April 15, 2022",
     content:
       "This app is a game changer for me. I am unable to go out always to buy medicinal products. Pharmeasy gives me the last liberty to shop essential healthcare products from home. The app is very user friendly and me being an elderly person do not find any difficulty in using it. They deliver well in time. Thanks😊"
   },
   {
     name: "Tirthankar Das Purkayastha",
-    date: "April 23, 2020",
+    date: "April 23, 2022",
     content:
       "Very good app. Would recommend it to everyone requiring fast and efficient delivery of medicinal products at the doorstep."
   },
   {
     name: "Debanjan Roy",
-    date: "April 18, 2020",
+    date: "April 18, 2022",
     content:
       "Excellent experience. Pharmeasy has not let it's customers down during lockdown. Thanks team. Great job. The application is also very smooth. And does its job well with an attractive UI and easy to use features. Good job developer."
   },
   {
     name: "Rohini Sarkar",
-    date: "April 21, 2020",
+    date: "April 21, 2022",
     content:
       "Very helpful and friendly app in terms of usability, customer support & money saving from the point of medical necessity of every person."
   },
   {
     name: "Varun Sonagra",
-    date: "April 23, 2020",
+    date: "April 23, 2022",
     content:
       "I've had a good experience. The consultation with a doctor feature works well too. The prices and discounts are great too. Overall the simplicity of the app is a plus too. I recommend this app. Will always be a customer/user now! Genuinely!"
   }
