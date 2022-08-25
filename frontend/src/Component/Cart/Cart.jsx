ddefc435936aa22cfbe837c617316ddae0acf3e5
SumitB1412 <beniwalsumit1412@gmail.com>
2022-08-25 20:29:56 +0530

    update billing box cart

--- frontend/src/Component/Cart/Cart.jsx
+++ frontend/src/Component/Cart/Cart.jsx
@@ -3,25 +3,55 @@ import React from "react";
 import styles from "./cart.module.css";
 import { FaGooglePlay } from "react-icons/fa";
 import { AiFillApple } from "react-icons/ai";
+import { FiChevronRight } from "react-icons/fi";
 import { SingleCartCard } from "../SingleCartCard/SingleCartCard";
 
 export const Cart = () => {
   return (
     <>
-    <div className={styles.cartWithItems}>
-      <div className={styles.singleCartItem}>
-        <div>
-          <h1>1 Item in Cart</h1>
-          <p>Prices are indicative</p>
+      <div className={styles.cartWithItems}>
+        <div className={styles.singleCartItem}>
+          <div>
+            <h1>1 Item in Cart</h1>
+            <p>Prices are indicative</p>
+          </div>
+          <div>
+            <SingleCartCard />
+            <SingleCartCard />
+            <SingleCartCard />
+          </div>
         </div>
-        <div>
-          <SingleCartCard/>
-          <SingleCartCard/>
-          <SingleCartCard/>
+        <div className={styles.cartBilling}>
+          <div className={styles.applyCoupon}>
+            <div>
+              <img
+                src="https://assets.pharmeasy.in/web-assets/dist/90a09c2f.svg"
+                alt="percent_img"
+              />{" "}
+              Apply Coupon
+            </div>
+            <FiChevronRight
+              fontSize="20px"
+              style={{ marginRight: "5%", fontWeight: "700" }}
+            />
+          </div>
+          <div className={styles.deliveryAddressBox}>Add Delivery Address</div>
+          <div className={styles.orderSummary}>
+            <p>Order Summary</p>
+            <div>
+              <p>Cart Value </p>
+              <span style={{ textDecoration: "line-through" , color:"#8897a2"}}>
+                ₹1316.00<strong style={{color:"#4f585e"}}>₹1176.80</strong>
+              </span>
+            </div>
+            <div>
+              <p>Amount to be paid</p>
+              <strong>₹1176.80</strong>
+            </div>
+          </div>
+          <div></div>
         </div>
       </div>
-      <div className={styles.cartBilling}></div>
-    </div>
       <div
         style={{
           width: "100%",
