416c16265439580a696bfd2ac2fddbb62a51eca0
SumitB1412 <beniwalsumit1412@gmail.com>
2022-08-25 19:04:04 +0530

    update single cart card

--- frontend/src/Component/SingleCartCard/SingleCartCard.jsx
+++ frontend/src/Component/SingleCartCard/SingleCartCard.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import styles from "./singlecartcard.module.css";
 
 export const SingleCartCard = () => {
-  return <div className={styles.singleCartWrapper}>
-    
-  </div>;
+  return (
+    <div className={styles.singleCartWrapper}>
+      <div className={styles.singleCartImgDiv}>
+        <img
+          src="https://cdn01.pharmeasy.in/dam/products_otc/181140/vicks-vaporub-50ml-relief-from-cold-cough-headache-and-body-pain-2-1654078748.jpg?dim=60x0&dpr=1&q=100"
+          alt=""
+        />
+      </div>
+      <div className={styles.singleCartInfo}>
+        <div className={styles.singleCartTitle}></div>
+        <p></p>
+        <div className={styles.singleCartQty}></div>
+        <p></p>
+      </div>
+    </div>
+  );
 };
