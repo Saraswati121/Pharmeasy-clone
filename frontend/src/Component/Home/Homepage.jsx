110f297f4f9c5e7fab207b5866a6d9c96ba368bd
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-08-25 12:33:56 +0530

    component created

--- frontend/src/Component/Home/Homepage.jsx
+++ frontend/src/Component/Home/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import style from './homepage.module.css'
 import Firstpart from './Firstpart';
 import Carousal from './Carousal';
+import Shop from './Shop';
 const Homepage = () => {
   return (
     <div>
         <div className={style.main_container}>
           <Firstpart/>
           <Carousal/>
+          <Shop/>
         </div>
     </div>
   )
---
+++ frontend/src/Component/Home/Newlaunches.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Newlaunches = () => {
+  return (
+    <div>Newlaunches</div>
+  )
+}
+
+export default Newlaunches
