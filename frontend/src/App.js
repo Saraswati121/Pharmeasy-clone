efed4ec6e3feb1c8cc9ef1efbc70aff141c428db
Saraswati121 <99686266+Saraswati121@users.noreply.github.com>
2022-08-27 22:50:07 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 
 import {AllRoutes} from '../src/Routes/AllRoutes'
-import {Offer} from "../src/Component/Offer/Offer"
+
 function App() {
   return (
     <div className="App">
-     {/* <AllRoutes/>  */}
-     <Offer/>
+     <AllRoutes/> 
      </div>
   );
 }
